22ff25e381bd290a9b1148d18b81164aed07b873
Igor Nikitin <igornik.spb@mail.ru>
2023-01-20 18:21:37 +0300

    I've compared 2 cats

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Равны ли ссылки на бины HelloWorld: " + (bean == bean2));
+        System.out.println("Равны ли ссылки на бины Cat: " + (beanCat1 == beanCat2));
     }
 }
